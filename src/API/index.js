181069481d916af4b0df975c1ee1ce4fdea4c73c
Pedro Alexander Garcia A <pedroalexandergarciaa@imac.myfiosgateway.com>
2023-08-09 20:50:10 -0400

    setting up API

---
+++ src/API/index.js
@@ -0,0 +1,5 @@
+// Construct and save your COHORT_NAME in a variable.
+// The predicate of the URL is https://strangers-things.herokuapp.com/ + /2004-UNF-HY-WEB-PT
+
+const COHORT_NAME = "2209-FTB-ET-WEB-FT";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
